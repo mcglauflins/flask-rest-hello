e59540e8a415a5e9c10764bc3f698f2d85b417b5
mcglauflins <sarahriebschlager@gmail.com>
2022-03-18 23:48:02 +0000

    commit

--- src/main.py
+++ src/main.py
@@ -9,7 +9,10 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+# from models import Person
+
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -20,26 +23,67 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
+
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+
 
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
+@app.route('/user', methods=['GET'])
+def user():
+    db.session.query(User).all()
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/main.py` is executed
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    email = request.json.get("email", None)
+    password = request.json.get("pass", None)
+    old_user = db.session.query(User).filter_by(email=email).one_or_none()
+    if old_user is None and all([email, password]):
+        new_user = User(email=email, password=password, is_active=True)
+        db.session.add(new_user)
+        db.session.commit()
+        response_body = {
+            "msg": "The user {} was created.".format(email)
+        }
+        return jsonify(response_body), 200
+    return jsonify({"msg": "Invalid request."}), 400
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("pass", None)
+    user = db.session.query(User).filter_by(email=email).one_or_none()
+    if user is not None:
+        if user.check_password_hash(password):
+            access_token = create_access_token(identity=email)
+            return jsonify(access_token=access_token)
+    return jsonify({"msg": "Invalid cedentials."}), 401
+
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.hybrid import hybrid_property
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,3 +20,60 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = generate_password_hash(password)
+
+    def check_password_hash(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    birth_year = db.Column(db.String(120), unique=True, nullable=True)
+    eye_color = db.Column(db.String(120), unique=True, nullable=True)
+    gender = db.Column(db.String(120), unique=True, nullable=True)
+    hair_color = db.Column(db.String(120), unique=True, nullable=True)
+    height = db.Column(db.String(120), unique=True, nullable=True)
+    mass = db.Column(db.String(120), unique=True, nullable=True)
+    skin_color = db.Column(db.String(120), unique=True, nullable=True)
+    homeworld = db.Column(db.String(120), unique=True, nullable=True)
+    films = db.Column(db.String(120), unique=True, nullable=True)
+    species = db.Column(db.String(120), unique=True, nullable=True)
+    starships = db.Column(db.String(120), unique=True, nullable=True)
+    vehicles = db.Column(db.String(120), unique=True, nullable=True)
+
+
+class Vehicles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    model = db.Column(db.String(120), unique=True, nullable=True)
+    vehicles = db.Column(db.String(120), unique=True, nullable=True)
+    manufacturer = db.Column(db.String(120), unique=True, nullable=True)
+    length = db.Column(db.String(120), unique=True, nullable=True)
+    cost_in_credits = db.Column(db.String(120), unique=True, nullable=True)
+    crew = db.Column(db.String(120), unique=True, nullable=True)
+    passengers = db.Column(db.String(120), unique=True, nullable=True)
+    max_atmosphering_speed = db.Column(
+        db.String(120), unique=True, nullable=True)
+    cargo_capacity = db.Column(db.String(120), unique=True, nullable=True)
+    consumables = db.Column(db.String(120), unique=True, nullable=True)
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    diameter = db.Column(db.String(120), unique=True, nullable=True)
+    rotation_period = db.Column(db.String(120), unique=True, nullable=True)
+    orbital_period = db.Column(db.String(120), unique=True, nullable=True)
+    gravity = db.Column(db.String(120), unique=True, nullable=True)
+    population = db.Column(db.String(120), unique=True, nullable=True)
+    climate = db.Column(db.String(120), unique=True, nullable=True)
+    terrain = db.Column(db.String(120), unique=True, nullable=True)
+    surface_water = db.Column(db.String(120), unique=True, nullable=True)
